77c3cf022f06e493c47f52a9422be8c20e1dde02
Sreekaamaakshi Rama <sreekaamaakshi777@gmail.com>
2026-09-14 11:00:05 +0400

    Add files via upload

---
+++ eligibility to vote.py
@@ -0,0 +1,6 @@
+#Eligibility to vote
+age = int(input("Enter age here: "))
+if age>=18:
+    print("You are an adult. You are eligible to vote")
+else:
+    print("You are a minor. You are NOT eligible to vote")
